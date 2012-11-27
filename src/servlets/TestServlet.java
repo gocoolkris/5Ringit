c0ddafbcecba770ed7a17c5160b86fdd0d3a9734
weidai0903 <weidai0903@gmail.com>
2012-11-27 16:38:56 -0500

    new3

--- src/servlets/TestServlet.java
+++ src/servlets/TestServlet.java
@@ -1,6 +1,8 @@
 package servlets;
 
+import java.util.ArrayList;
 
+import object.Post;
 
 import org.apache.velocity.Template;
 import org.apache.velocity.servlet.VelocityServlet;
@@ -13,16 +15,31 @@ public class TestServlet extends VelocityServlet {
 	   public Template handleRequest( HttpServletRequest request,
                HttpServletResponse response,
                Context context ) {
+		   
+		    Post p=new Post("2008 summer olympic"," ","http://en.wikipedia.org/wiki/2008_Summer_Olympics");
+			Post p2=new Post("2012 summer olympic"," ","http://en.wikipedia.org/wiki/2012_Summer_Olympics");
+			Post p3=new Post("2016 summer olympic"," ","http://en.wikipedia.org/wiki/2016_Summer_Olympics");
+			
+			ArrayList <Post> posts = new ArrayList<Post>();
+			posts.add(p);
+			posts.add(p2);
+			posts.add(p3);
+			
+			
 
 				Template template = null;
 				
 				try {
-				context.put("name", "Velocity Test");
-				template = Velocity.getTemplate("hello.vm");
+						
+					
+					context.put("posts", posts);
+					//context.put("des", p.getDesc());
+					//p.getDesc();
+					template = Velocity.getTemplate("hello.vm");
 				
 				
 				} catch( Exception e ) {
-				System.err.println("Exception caught: " + e.getMessage());
+					System.err.println("Exception caught: " + e.getMessage());
 				}
 				
 				return template;
