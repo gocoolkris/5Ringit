8184575ffee1c112c331e0b750cc1cedd42d8e24
weidai0903 <weidai0903@gmail.com>
2012-12-01 14:56:07 -0500

    awsome servlets

--- src/object/Post.java
+++ src/object/Post.java
@@ -1,20 +1,64 @@
 package object;
 
 import java.io.Serializable;
+import java.util.Date;
 
 public class Post implements Serializable{
 	private int id;		
 	private String title;		
 	private String desc;	
-	private String link;	
+	private String link;
+	private String author;
+	private long postedTime;
+	private int commentCount;
+	private int likeCount;
 
 	public Post(){}
-	public Post(String title,String desc,String url){
+	public Post(String title,String desc,String url,String author){
 		this.title=title;
 		this.desc=desc;
 		this.link=url;
+		this.author = author;
+		this.commentCount = 0;
+		this.likeCount = 0;
 
 	}
+	/**
+	 * returns the number of days from now to postedTime
+	 * @return
+	 */
+	public long getRalativePostedTime() {
+		
+		Date date = new Date();
+		long now = date.getTime();
+		long relativeTime = now - postedTime;		
+		return relativeTime/1000/3600;
+		
+	}
+	/**
+	 * set the postedTime by now
+	 */
+	public void setPostedTime() {
+		Date date = new Date();
+		long now = date.getTime();
+		this.postedTime = now;
+		
+	}
+	
+	public int getCommentCount() {
+		return commentCount;
+	}
+	public void setCommentCount(int commentCount) {
+		this.commentCount = commentCount;
+	}
+	
+	public int getLikeCount() {
+		return likeCount;
+	}
+	public void setLikeCount(int likeCount) {
+		this.likeCount = likeCount;
+	}
+	
 	public int getId() {
 		return id;
 	}
@@ -31,6 +75,9 @@ public class Post implements Serializable{
 	public String getTitle() {
 		return title;
 	}
+	public String getAuthor() {
+		return author;
+	}
 	public void setTitle(String title) {
 		this.title = title;
 	}
@@ -40,5 +87,6 @@ public class Post implements Serializable{
 	public void setLink(String url) {
 		this.link = url;
 	}		
+	
 
 }
