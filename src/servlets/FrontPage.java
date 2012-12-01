61968f382a23c8e455732f84f8ee9a860c617da9
weidai0903 <weidai0903@gmail.com>
2012-12-01 15:40:48 -0500

    posts

--- src/servlets/FrontPage.java
+++ src/servlets/FrontPage.java
@@ -13,32 +13,11 @@ import javax.servlet.http.*;
 
 public class FrontPage extends VelocityServlet {
 
-	   public Template handleRequest( HttpServletRequest request,
-               HttpServletResponse response,
-               Context context ) {
 	 public Template handleRequest( HttpServletRequest request,
            HttpServletResponse response,
             	Context context ) {
 		   
 		   int numPostsPerPage = 5;
-		    Post p=new Post("2008 summer olympic"," description1","http://en.wikipedia.org/wiki/2008_Summer_Olympics","david");
-			Post p2=new Post("2012 summer olympic"," hello world","http://en.wikipedia.org/wiki/2012_Summer_Olympics", "gokul");
-			Post p3=new Post("2016 summer olympic","foo bar","http://en.wikipedia.org/wiki/2016_Summer_Olympics", "yang");
-			
-			ArrayList <Post> posts = new ArrayList<Post>();
-			posts.add(p);
-			posts.add(p2);
-			posts.add(p3);
-			
-			p.setId(1);
-			p2.setId(2);
-			p3.setId(3);
-			
-			p.setPostedTime();
-			p2.setPostedTime();
-			p3.setPostedTime();
-			
-			int totalPages = posts.size()/numPostsPerPage;
 		   
 		   Post[] posts = new Post[11];
 		   
@@ -65,8 +44,6 @@ public class FrontPage extends VelocityServlet {
 					context.put("currentPage", 1);
 					context.put("totalPages", totalPages);
 					
-					//context.put("des", p.getDesc());
-					//p.getDesc();
 					template = Velocity.getTemplate("index.html");
 				
 				
